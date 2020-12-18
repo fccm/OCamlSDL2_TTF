8cc0f01f1f769a9c2a85c82f25c7114d0f3a3a71
Florent Monnier <monnier.florent@gmail.com>
2020-12-18 13:03:34 +0100

    removed naked pointer

--- src/sdlttf_stub.h
+++ src/sdlttf_stub.h
@@ -1,7 +1,14 @@
 #ifndef _CAML_SDL_TTF_
 #define _CAML_SDL_TTF_
 
-#define TTF_Font_val(font) ((TTF_Font *)(font))
-#define Val_TTF_Font(font) ((value)(font))
+TTF_Font * TTF_Font_val(value font)
+{
+    return (TTF_Font *) Nativeint_val(font);
+}
+
+value Val_TTF_Font(TTF_Font * p)
+{
+    return caml_copy_nativeint((intnat) p);
+}
 
 #endif /* _CAML_SDL_TTF_ */
